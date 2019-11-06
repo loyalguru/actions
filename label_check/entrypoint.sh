0620667de04942227308e1d7ddc820b7b12868f8
Eric Ponce <tricokun@gmail.com>
2019-11-06 11:24:43 +0100

    add chat

--- label_check/entrypoint.sh
+++ label_check/entrypoint.sh
@@ -41,10 +41,12 @@ main(){
         exit 1
     fi
 
+
+
     chat=$(curl -X POST \
-  "https://chat.googleapis.com/v1/spaces/AAAAKMO_ki8/messages?key=${INPUT_CKEY}&token=${INPUT_CTOKEN}" \
-  -H 'Content-Type: application/json' \
-  -d '{"text" : "⚡ Atención acción de deploy lanzada por ${GITHUB_ACTOR} en el PR ${title} del proyecto ${GITHUB_REPOSITORY} ⚡"}')
+    "https://chat.googleapis.com/v1/spaces/AAAAKMO_ki8/messages?key=${INPUT_CKEY}&token=${INPUT_CTOKEN}" \
+    -H 'Content-Type: application/json' \
+    -d "{\"text\" : \"⚡ Atención acción de deploy lanzada por ${GITHUB_ACTOR} en el PR ${title} del proyecto ${GITHUB_REPOSITORY} ⚡\"}")
 
     echo ${chat}
 
