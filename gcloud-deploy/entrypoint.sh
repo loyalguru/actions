9863c6f071fe45db75e0b55d76b05c71877f6501
Andres D. Molins <adiaz@loyal.guru>
2019-11-21 10:47:29 +0100

    Added 2 new deploy needed actions.

---
+++ gcloud-deploy/entrypoint.sh
@@ -0,0 +1,80 @@
+#!/bin/sh
+
+set -e
+
+main(){
+
+  number=$(jq --raw-output .number ${GITHUB_EVENT_PATH})
+  issue=$(curl -X GET "https://api.github.com/repos/${GITHUB_REPOSITORY}/issues/${number}" \
+    -H "Authorization: token ${INPUT_TOKEN}")
+  title=$(echo "${issue}" | jq -r .title)
+
+  if [ ! -d "$HOME/.config/gcloud" ]; then
+     if [ -z "${APPLICATION_CREDENTIALS-}" ]; then
+        echo "APPLICATION_CREDENTIALS not found. Exiting...."
+
+        chat=$(curl -s -X POST \
+        "https://chat.googleapis.com/v1/spaces/${INPUT_SPACE}/messages?key=${INPUT_CKEY}&token=${INPUT_CTOKEN}" \
+        -H 'Content-Type: application/json' \
+        -d "{\"text\" : \"🚫 DEPLOY: Deploy action failed. 🛂 APPLICATION_CREDENTIALS not found. Exiting.... \
+            Deployer: *${GITHUB_ACTOR}*. PR: *${title}*. Project: *${GITHUB_REPOSITORY}* 🚫\"}")
+
+        exit 1
+     fi
+
+     if [ -z "${PROJECT_ID-}" ]; then
+        echo "PROJECT_ID not found. Exiting...."
+
+        chat=$(curl -s -X POST \
+        "https://chat.googleapis.com/v1/spaces/${INPUT_SPACE}/messages?key=${INPUT_CKEY}&token=${INPUT_CTOKEN}" \
+        -H 'Content-Type: application/json' \
+        -d "{\"text\" : \"🚫 DEPLOY: Deploy action failed. 🛂 PROJECT_ID not found. Exiting.... \
+            Deployer: *${GITHUB_ACTOR}*. PR: *${title}*. Project: *${GITHUB_REPOSITORY}* 🚫\"}")
+
+        exit 1
+     fi
+
+     echo "$APPLICATION_CREDENTIALS" | base64 -d > /tmp/account.json
+
+     gcloud auth activate-service-account --key-file=/tmp/account.json
+     gcloud config set project "$PROJECT_ID"
+  fi
+
+  echo ::add-path::/google-cloud-sdk/bin/gcloud
+  echo ::add-path::/google-cloud-sdk/bin/gsutil
+
+  chat=$(curl -s -X POST \
+    "https://chat.googleapis.com/v1/spaces/${INPUT_SPACE}/messages?key=${INPUT_CKEY}&token=${INPUT_CTOKEN}" \
+    -H 'Content-Type: application/json' \
+    -d "{\"text\" : \"🔄 DEPLOY: Starting deploy... \
+        Deployer: *${GITHUB_ACTOR}*. PR: *${title}*. Project: *${GITHUB_REPOSITORY}* ⭐\"}")
+
+  command="gcloud app deploy app.yml"
+  sh -c "$command"
+  status=$?
+
+  if [ $status -eq 0 ]; then
+    echo "...done"
+    echo ""
+    echo ""
+
+    chat=$(curl -s -X POST \
+    "https://chat.googleapis.com/v1/spaces/${INPUT_SPACE}/messages?key=${INPUT_CKEY}&token=${INPUT_CTOKEN}" \
+    -H 'Content-Type: application/json' \
+    -d "{\"text\" : \"✅ DEPLOY: Deploy action finished succeed! 🎉🎉 \
+        Deployer: *${GITHUB_ACTOR}*. PR: *${title}*. Project: *${GITHUB_REPOSITORY}* ⭐\"}")
+
+  else
+    echo "...error!"
+    echo ""
+    echo ""
+
+    chat=$(curl -s -X POST \
+      "https://chat.googleapis.com/v1/spaces/${INPUT_SPACE}/messages?key=${INPUT_CKEY}&token=${INPUT_CTOKEN}" \
+      -H 'Content-Type: application/json' \
+      -d "{\"text\" : \"🚫 DEPLOY: Deploy action failed. Please go to project *${GITHUB_REPOSITORY}* -> Actions to see the errors. \
+          Deployer: *${GITHUB_ACTOR}*. PR: *${title}*. Project: *${GITHUB_REPOSITORY}* 🚫\"}")
+
+    exit 1
+  fi
+}
---
+++ generate_yaml/entrypoint.sh
@@ -0,0 +1,76 @@
+#!/bin/sh -l
+
+set -e
+
+main(){
+
+  ready="false"
+  token=$INPUT_RAILSTOKEN
+  
+  number=$(jq --raw-output .number ${GITHUB_EVENT_PATH})
+  issue=$(curl -X GET "https://api.github.com/repos/${GITHUB_REPOSITORY}/issues/${number}" \
+    -H "Authorization: token ${INPUT_TOKEN}")
+  echo "DEBUG {\"title\":\"${labels}\", \"head\":\"${branch}\", \"base\": \"staging\"}"
+  title=$(echo "${issue}" | jq -r .title)
+
+  echo "-------------------------------------"
+  echo "-------------------------------------"
+  echo "||                                 ||"
+  echo "||      Generating YAML file       ||"
+  echo "||                                 ||"
+  echo "-------------------------------------"
+  echo "-------------------------------------"
+
+  echo ""
+  echo "Started at $(date)"
+  echo ""
+  echo ""
+
+  # Generate app.yml file
+  
+  echo "STEP 1 OF 1: Generating app.yml file..."
+
+  FILE=app_example.yml
+  if test -f "$FILE"; then
+      ready = "true"
+  fi
+
+  if [ "$ready" = "true" ]; then
+    cp -vap app_example.yml app.yml
+    sed -i "" "s/\${{ SECRET_KEY }}/\"${INPUT_SECRET_KEY}\"/g" $FILE
+    sed -i "" "s/\${{ DB_FULL_URL }}/\"${INPUT_DB_FULL_URL}\"/g" $FILE
+    sed -i "" "s/\${{ REDIS_CACHE_USER }}/'${INPUT_REDIS_CACHE_USER}'/g" $FILE
+    sed -i "" "s/\${{ REDIS_CACHE_PASSWORD }}/'${INPUT_REDIS_CACHE_PASSWORD}'/g" $FILE
+    sed -i "" "s/\${{ REDIS_SIDEKIQ_USER }}/'${REDIS_SIDEKIQ_USER}'/g" $FILE
+    sed -i "" "s/\${{ REDIS_SIDEKIQ_PASSWORD }}/'${REDIS_SIDEKIQ_PASSWORD}'/g" $FILE
+    sed -i "" "s/\${{ AWS_ACCESS_KEY_ID }}/'${INPUT_AWS_ACCESS_KEY_ID}'/g" $FILE
+    sed -i "" "s/\${{ AWS_SECRET_ACCESS_KEY }}/'${INPUT_AWS_SECRET_ACCESS_KEY}'/g" $FILE
+    sed -i "" "s/\${{ DATADOG_API_KEY }}/'${INPUT_DATADOG_API_KEY}'/g" $FILE
+    sed -i "" "s/\${{ SENTRY_DSN }}/'${INPUT_SENTRY_DSN}'/g" $FILE
+    sed -i "" "s/\${{ GOOGLE_PUBSUB_CREDENTIALS }}/'${GOOGLE_PUBSUB_CREDENTIALS}'/g" $FILE
+  else
+    echo "${FILE} file don't exist."
+
+    chat=$(curl -s -X POST \
+    "https://chat.googleapis.com/v1/spaces/${INPUT_SPACE}/messages?key=${INPUT_CKEY}&token=${INPUT_CTOKEN}" \
+    -H 'Content-Type: application/json' \
+    -d "{\"text\" : \"🚫 GENERATE YAML: failed to generate the file, ${FILE} file don't exist. \
+        Deployer: *${GITHUB_ACTOR}*. PR: *${title}*. Project: *${GITHUB_REPOSITORY}* 🚫\"}")
+
+    exit 1
+  fi
+
+  echo "..done"
+  echo ""
+  echo ""
+
+  chat=$(curl -s -X POST \
+  "https://chat.googleapis.com/v1/spaces/${INPUT_SPACE}/messages?key=${INPUT_CKEY}&token=${INPUT_CTOKEN}" \
+  -H 'Content-Type: application/json' \
+  -d "{\"text\" : \"⭐ DEPLOY: Starting deploy...\
+      Deployer: *${GITHUB_ACTOR}*. PR: *${title}*. Project: *${GITHUB_REPOSITORY}* ⭐\"}")
+
+  echo "READY TO DEPLOY"
+}
+
+main "$@"
