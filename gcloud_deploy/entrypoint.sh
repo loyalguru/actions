74cb27fc79fa8183791eca8f1d346a6166244765
Andres D. Molins <adiaz@loyal.guru>
2019-11-21 16:37:01 +0100

    Try to update PATH variable first.

--- gcloud_deploy/entrypoint.sh
+++ gcloud_deploy/entrypoint.sh
@@ -26,8 +26,8 @@ main(){
 
   printenv
 
-  echo ::add-path::/google-cloud-sdk/bin/gcloud
-  echo ::add-path::/google-cloud-sdk/bin/gsutil
+  # echo ::add-path::/google-cloud-sdk/bin/gcloud
+  # echo ::add-path::/google-cloud-sdk/bin/gsutil
 
   if [ ! -d "$HOME/.config/gcloud" ]; then
      if [ -z "${application_credentials}" ]; then
