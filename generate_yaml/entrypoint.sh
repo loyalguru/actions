910758721f8d209e2782dc889552994515f3d2b8
Andres D. Molins <adiaz@loyal.guru>
2020-09-15 15:13:59 +0200

    Add new SECRET_KEY_ALT env variable.

--- generate_yaml/entrypoint.sh
+++ generate_yaml/entrypoint.sh
@@ -81,6 +81,7 @@ main(){
       launchdarkly_sdkkey=$INPUT_LAUNCHDARKLY_SDKKEY_STAGING
     fi
     sed -i -e "s#@SECRET_KEY@#\"${INPUT_SECRET_KEY}\"#g" $FILE
+    sed -i -e "s#@SECRET_KEY_ALT@#\"${INPUT_SECRET_KEY_ALT}\"#g" $FILE
     sed -i -e "s#@DB_FULL_URL@#${db_full_url}#g" $FILE
     sed -i -e "s/@REDIS_CACHE_USER@/'${INPUT_REDIS_CACHE_USER}'/g" $FILE
     sed -i -e "s/@REDIS_CACHE_PASSWORD@/'${INPUT_REDIS_CACHE_PASSWORD}'/g" $FILE
